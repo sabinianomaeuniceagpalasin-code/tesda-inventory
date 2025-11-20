d81c135af8f817444f9c3e4b0f406950f703ca75
Danielle <daniell07720@gmail.com>
2025-11-21 02:11:55 +0800

    update database

--- database/tesda_db.sql
+++ database/tesda_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 17, 2025 at 10:31 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: Nov 20, 2025 at 06:11 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.3.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tesda`
+-- Database: `tesda_db`
 --
 
 -- --------------------------------------------------------
@@ -28,12 +28,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `failed_jobs` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `connection` text NOT NULL,
-  `queue` text NOT NULL,
-  `payload` longtext NOT NULL,
-  `exception` longtext NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `id` bigint UNSIGNED NOT NULL,
+  `connection` text COLLATE utf8mb4_general_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_general_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -43,14 +43,14 @@ CREATE TABLE `failed_jobs` (
 --
 
 CREATE TABLE `issued_log` (
-  `id` int(11) NOT NULL,
-  `student_name` varchar(255) NOT NULL,
-  `serial_no` varchar(255) NOT NULL,
-  `property_no` varchar(255) NOT NULL,
-  `form_type` enum('ICS','PAR') NOT NULL,
+  `id` int NOT NULL,
+  `student_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `serial_no` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `property_no` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `form_type` enum('ICS','PAR') COLLATE utf8mb4_general_ci NOT NULL,
   `issued_date` date NOT NULL,
   `return_date` date DEFAULT NULL,
-  `reference_no` varchar(255) NOT NULL,
+  `reference_no` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -70,22 +70,43 @@ INSERT INTO `issued_log` (`id`, `student_name`, `serial_no`, `property_no`, `for
 --
 
 CREATE TABLE `issued_summary` (
-  `id` bigint(20) NOT NULL,
-  `property_no` varchar(20) NOT NULL,
-  `item_count` int(11) NOT NULL DEFAULT 1,
-  `student_name` varchar(100) NOT NULL,
-  `form_type` varchar(10) NOT NULL,
-  `status` enum('Active','Archive') NOT NULL DEFAULT 'Active',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `id` bigint NOT NULL,
+  `property_no` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `item_count` int NOT NULL DEFAULT '1',
+  `student_name` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `issued_by` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `form_type` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `reference_no` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` enum('Active','Archive') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'Active',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `issued_summary`
 --
 
-INSERT INTO `issued_summary` (`id`, `property_no`, `item_count`, `student_name`, `form_type`, `status`, `created_at`, `updated_at`) VALUES
-(1, '00001', 2, 'Cedric John P. Sadsad', 'PAR', 'Active', '2025-11-14 23:36:04', '2025-11-14 23:36:04');
+INSERT INTO `issued_summary` (`id`, `property_no`, `item_count`, `student_name`, `issued_by`, `form_type`, `reference_no`, `status`, `created_at`, `updated_at`) VALUES
+(1, '00001', 2, 'Cedric John P. Sadsad', '', 'PAR', '1', 'Active', '2025-11-14 23:36:04', '2025-11-18 16:22:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `maintenance_records`
+--
+
+CREATE TABLE `maintenance_records` (
+  `id` int UNSIGNED NOT NULL,
+  `property_no` varchar(255) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `issue_problem` text NOT NULL,
+  `date_reported` date NOT NULL,
+  `repair_cost` decimal(10,2) DEFAULT NULL,
+  `expected_completion` date DEFAULT NULL,
+  `remarks` text,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -94,9 +115,9 @@ INSERT INTO `issued_summary` (`id`, `property_no`, `item_count`, `student_name`,
 --
 
 CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) NOT NULL,
-  `batch` int(11) NOT NULL
+  `id` int UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -106,8 +127,8 @@ CREATE TABLE `migrations` (
 --
 
 CREATE TABLE `password_resets` (
-  `email` varchar(255) NOT NULL,
-  `token` varchar(255) NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -118,12 +139,12 @@ CREATE TABLE `password_resets` (
 --
 
 CREATE TABLE `personal_access_tokens` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `tokenable_type` varchar(255) DEFAULT NULL,
-  `tokenable_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `token` varchar(64) DEFAULT NULL,
-  `abilities` text DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `tokenable_id` bigint UNSIGNED DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `token` varchar(64) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `abilities` text COLLATE utf8mb4_general_ci,
   `last_used_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -136,11 +157,11 @@ CREATE TABLE `personal_access_tokens` (
 --
 
 CREATE TABLE `property_inventory` (
-  `id` int(11) NOT NULL,
-  `property_no` varchar(255) NOT NULL,
-  `tool_name` varchar(255) NOT NULL,
-  `quantity` int(11) NOT NULL DEFAULT 0,
-  `unit_cost` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `id` int NOT NULL,
+  `property_no` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `tool_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `quantity` int NOT NULL DEFAULT '0',
+  `unit_cost` decimal(10,2) NOT NULL DEFAULT '0.00',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -150,8 +171,8 @@ CREATE TABLE `property_inventory` (
 --
 
 INSERT INTO `property_inventory` (`id`, `property_no`, `tool_name`, `quantity`, `unit_cost`, `created_at`, `updated_at`) VALUES
-(1, '00001', 'Printer', 10, 15000.00, NULL, '2025-11-14 20:19:38'),
-(2, '00002', 'Photocopier', 25, 300.00, NULL, '2025-11-16 04:43:23');
+(1, '00001', 'Printer', 10, '15000.00', NULL, '2025-11-14 20:19:38'),
+(2, '00002', 'Photocopier', 25, '300.00', NULL, '2025-11-16 04:43:23');
 
 -- --------------------------------------------------------
 
@@ -160,12 +181,12 @@ INSERT INTO `property_inventory` (`id`, `property_no`, `tool_name`, `quantity`, 
 --
 
 CREATE TABLE `sessions` (
-  `id` varchar(255) NOT NULL,
-  `user_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `ip_address` varchar(45) DEFAULT NULL,
-  `user_agent` text DEFAULT NULL,
-  `payload` longtext NOT NULL,
-  `last_activity` int(11) NOT NULL
+  `id` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `user_id` bigint UNSIGNED DEFAULT NULL,
+  `ip_address` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `user_agent` text COLLATE utf8mb4_general_ci,
+  `payload` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `last_activity` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -173,9 +194,10 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('lchZtiRDXQeOflcUjmKkuhM5udw3Zz4ZFGHoVa0N', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoid2NaUm1hcDkwVlI0QldzMFVwVllWWnlXOTc2VWJxMnVsSWpUY0tRMCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYXNoYm9hcmQiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO30=', 1763371718),
-('LuoBd5dnEXEbJzfnc2cbZL9K9JWkXM7SVkjHP9mQ', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiWkp5RW9DMWJhOHdlN2l1ZDR3VE9MOUI3R05KMUpaU2gyTFUyRnhZYiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYXNoYm9hcmQiO31zOjM6InVybCI7YToxOntzOjg6ImludGVuZGVkIjtzOjMxOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvZGFzaGJvYXJkIjt9czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTt9', 1763208834),
-('rj3KizxBhk7B1dzUY0zWaiq12Y0tbA5gdWbYA7A1', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoia1Bac3dWbjB4NzFCVXNRekpxdVBkOUE0eWpUdzJ5c3pYbFpaVEh3TCI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sb2dpbiI7fX0=', 1763297749);
+('c35kNVTLoHX8J9xGsQGXPr192iVo4yMbJ7dXKjPX', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiMVg3d3Qxc0N6QzZESzNYbnYxbGQzWmdPeUtIQW9KYWZyRDVHQ1NQcSI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYXNoYm9hcmQiO31zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO30=', 1763585677),
+('cJb7ZcKL1xbIjqLoYsI3SmeMmK0AQVPbyaJ6qxph', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoib1czaUdMczQ1UFVIWHRxendmeFlVb3NNU0I1NWNsQXZ0UkpHYk8yNyI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czozMToiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2Rhc2hib2FyZCI7fXM6OToiX3ByZXZpb3VzIjthOjE6e3M6MzoidXJsIjtzOjI3OiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvbG9naW4iO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX19', 1763624796),
+('TRfCJlpZBKNy7CSWaPLt2We5PH7KfflFHOkLDZW8', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiMFhqd3JWWlh6NEs1d25ZRldld2E3eUN4UXllMjZMdHhsRGxkMTZrNSI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYXNoYm9hcmQiO31zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO30=', 1763639190),
+('wO1uIzhNKXenUsiMwtfywmQOvY8WgHl2cDgSAxXd', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiWE94enYxSXFNSXFLSWFhR2FJQk82aklSRWo0Y3c2QkhyVEVTZUx3YyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYXNoYm9hcmQiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjM6InVybCI7YToxOntzOjg6ImludGVuZGVkIjtzOjMxOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvZGFzaGJvYXJkIjt9czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTt9', 1763660153);
 
 -- --------------------------------------------------------
 
@@ -184,10 +206,10 @@ INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, 
 --
 
 CREATE TABLE `student` (
-  `id` int(11) NOT NULL,
-  `student_name` varchar(255) NOT NULL,
-  `student_number` varchar(255) NOT NULL,
-  `batch` varchar(255) NOT NULL,
+  `id` int NOT NULL,
+  `student_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `student_number` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `batch` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -206,16 +228,16 @@ INSERT INTO `student` (`id`, `student_name`, `student_number`, `batch`, `created
 --
 
 CREATE TABLE `tools` (
-  `id` int(11) NOT NULL,
-  `tool_name` varchar(255) NOT NULL,
-  `classification` varchar(255) NOT NULL,
-  `source_of_fund` varchar(255) NOT NULL,
+  `id` int NOT NULL,
+  `tool_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `classification` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `source_of_fund` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `date_acquired` date NOT NULL,
-  `property_no` varchar(255) NOT NULL,
-  `serial_no` varchar(255) NOT NULL,
-  `stock` int(11) DEFAULT 1,
-  `remarks` text DEFAULT NULL,
-  `status` enum('Available','Borrowed','Issued','For Repair','Damaged','Lost') DEFAULT 'Available',
+  `property_no` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `serial_no` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `stock` int DEFAULT '1',
+  `remarks` text COLLATE utf8mb4_general_ci,
+  `status` enum('Available','Borrowed','Issued','For Repair','Damaged','Lost') COLLATE utf8mb4_general_ci DEFAULT 'Available',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -268,19 +290,19 @@ INSERT INTO `tools` (`id`, `tool_name`, `classification`, `source_of_fund`, `dat
 --
 
 CREATE TABLE `users` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) NOT NULL,
+  `id` bigint UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) NOT NULL,
-  `remember_token` varchar(100) DEFAULT NULL,
-  `full_name` varchar(255) DEFAULT NULL,
-  `contact_no` varchar(50) DEFAULT NULL,
-  `role` varchar(50) DEFAULT NULL,
-  `verification_code` varchar(10) DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `full_name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `contact_no` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `role` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `verification_code` varchar(10) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `code_expires_at` datetime DEFAULT NULL,
-  `is_verified` tinyint(1) DEFAULT 0,
-  `is_approved` tinyint(1) DEFAULT 0,
+  `is_verified` tinyint(1) DEFAULT '0',
+  `is_approved` tinyint(1) DEFAULT '0',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -313,6 +335,12 @@ ALTER TABLE `issued_log`
 -- Indexes for table `issued_summary`
 --
 ALTER TABLE `issued_summary`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `maintenance_records`
+--
+ALTER TABLE `maintenance_records`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -375,55 +403,61 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `issued_log`
 --
 ALTER TABLE `issued_log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `issued_summary`
 --
 ALTER TABLE `issued_summary`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `maintenance_records`
+--
+ALTER TABLE `maintenance_records`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `property_inventory`
 --
 ALTER TABLE `property_inventory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tools`
 --
 ALTER TABLE `tools`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
